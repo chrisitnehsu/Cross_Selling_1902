bc6b197b38fec75c7c276c20f5d452301ea0ca0e
helloklima <helloklima@gmail.com>
2019-03-13 10:42:35 +0800

    0312

--- testSMOTE.R
+++ testSMOTE.R
@@ -1,3 +1,3 @@
-data <- SMOTE(if_ticket_success~., data = all_data_ticket, perc.over = 600, perc.under = 180)
+data <- SMOTE(if_ticket_success~., data = all_data_ticket, perc.over = 600, perc.under = 400)
 summary(data$if_ticket_success) 
 sum(data$if_ticket_success == "success") / nrow(data)
