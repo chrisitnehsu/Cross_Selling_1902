636dbaf9ee64791b353ab5276c0f0d6764f17467
Marvin Chuang <helloklima@gmail.com>
2019-03-27 19:07:26 +0800

    Update 4a_Single_Model_Reasearch.R

--- 4a_Single_Model_Reasearch.R
+++ 4a_Single_Model_Reasearch.R
@@ -1,8 +1,9 @@
+model_choose <- "svmRadial"
 set.seed(13)
 train_index <- createFolds(all_data_ticket$if_ticket_success, k = 4, returnTrain = T)
-model_choose <- "svmRadial"
 train_data <- all_data_ticket[train_index[[1]],]
 test_data <- all_data_ticket[-train_index[[1]],]
+
 
 function_modeling <- function(model_choose){
 set.seed(13)
@@ -38,7 +39,7 @@ conf <- confusionMatrix(pred, processed_test_data$if_ticket_success, mode = "eve
 pred <- predict(model, processed_test_data,  type = "prob")
 AUC <- colAUC(pred, processed_test_data[["if_ticket_success"]])
 AUC
-
+#
 metrics <- as.vector(cbind(conf$overall[["Accuracy"]]
                            ,conf$byClass[["Sensitivity"]]
                            ,conf$byClass[["Precision"]]
@@ -60,5 +61,10 @@ for(i in 1:length(train_index)){
 performance$avg <- rowMeans(performance)
 performance %>% View
 
-
-test_data_w_target <- cbind(test_data, pred$success)
+#
+test_data_w_target <- data.frame(pred$success, test_data)
+test_data_w_target %>% group_by(if_ticket_success) %>% summarise(avg_pred = mean(pred.success))
+test_data_w_target$cut <- cut(test_data_w_target$pred.success, c(0,0.33,0.66,1))
+t1 <- test_data_w_target %>% group_by(cut) %>% filter(if_ticket_success == "success") %>% summarise(rate = n())
+t2 <- test_data_w_target %>% group_by(cut) %>% summarise(rate = n())
+t1$rate/t2$rate
